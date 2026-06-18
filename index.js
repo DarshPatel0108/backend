178efe03b46c40c68e77ad6481c7feed8fdd7c09
DarshPatel0108 <darshpatel1890@gmail.com>
2026-06-18 23:14:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/login',(req,res)=>{
+  res.send('Login Page');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
+
